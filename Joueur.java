5867ab6cc4327f125e6b6e67a1b450dc52d96f40
Vincent Frochot <vincentfrochot@live.fr>
2010-04-24 19:13:12 +0200

    gestion de partie

--- Joueur.java
+++ Joueur.java
@@ -1,22 +1,41 @@
+import java.io.*;
+import java.lang.*;
+
 public class Joueur {
 	String pseudonyme;
+	BufferedReader br;
+	public byte score;
+	Mouvement coupChoisi;
+	
 	public Joueur() {
 		
 	}
 	
 	public Joueur(String pseudo) {
 		this.pseudonyme = pseudo;
+		System.out.println(pseudo+" a rejoint la partie.");
+		br = new BufferedReader(new InputStreamReader(System.in));
 	}
-/*
- * @author Vincent
- * @return boolean
- * <br />
- * Ici on envoie simplement le mouvement au serveur Partie pour qu'il le prenne en considération puis envoie l'ordre à chaque des clients.
- */
-	public boolean Jouer(Mouvement[] m) {
-/*
-	Un deplacement revient a appliquer un vecteur commun sur un ensemble de billes.
-*/
-		return true;
+	public Mouvement jouer(/*Mouvement[] coupsPossibles*/) {
+		System.out.println("Liste des coups possibles : ");
+		/*
+		for(Mouvement[] coupsPossibles : coupPossible)
+			System.out.println("1ere:"+coupPossible.getPremiere()+" 2e:"+coupPossible.getDerniere()+" vecteur:"+coupPossible.getVecteur());
+		*/
+		try {
+			System.out.print("Premiere bille : ");
+			byte premiere = new Byte(br.readLine());
+			System.out.print("Seconde bille : ");
+			byte seconde = new Byte(br.readLine());
+			System.out.print("Vecteur : ");
+			byte vecteur = new Byte(br.readLine());
+			System.out.println("mouvement choisi : ");
+			coupChoisi = new Mouvement(premiere, seconde, vecteur);
+			return coupChoisi;		
+		}
+		catch (IOException e) {
+			System.err.println("Joueur::jouer() -> erreur de lecture du mouvement a la ligne de cmd.");	
+		}
+		return new Mouvement((byte)1, (byte)1, (byte)1);
 	}
 }
